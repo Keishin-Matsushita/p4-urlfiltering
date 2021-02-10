46ddd8f9cead8653d357251c21f0fd68e37ef047
Keishin "spicy" Matsushita <78808520+Keishin-Matsushita@users.noreply.github.com>
2021-02-10 21:19:01 +0900

    Add files via upload

--- include/url.py
+++ include/url.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 
 urls = [
-	[ "0x0a000002","GET / HTTP/1.1" ],
-	[ "0x0a000003","GET /index.html HTTP/1.1" ],
-	[ "0x0a000002","GET /hello.html HTTP/1.1" ]
+	[ "0x0a000002","/" ],
+	[ "0x0a000003","/index.html" ],
+	[ "0x0a000002","/hello.html" ]
 ]
 
 for ui in range(len(urls)):
 	ipaddr,url = urls[ui]
-	url = url + chr(0x0d) + chr(0x0a)
 	l = len(url)
+	print( "// URI:%s" % (url) )
 	print( "(%s," % (ipaddr),end="" )
 	for i in range(l):
 		print( "0x%x," % (ord(url[i])),end="")
