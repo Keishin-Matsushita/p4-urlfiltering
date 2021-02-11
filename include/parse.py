9075634a5268cc65b637d6e0c85321d3379838f6
Keishin "spicy" Matsushita <78808520+Keishin-Matsushita@users.noreply.github.com>
2021-02-11 19:07:42 +0900

    Delete parse.py

--- include/parse.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-
-for i in range(32):
-	print( "\t\thdr.http_url[%d].setValid();" % (i) )
-	print( "\t\thdr.http_url[%d].ch = 0;" % (i) )
-	print( "\t\thdr.http_url[%d].setInvalid();" % (i) )
